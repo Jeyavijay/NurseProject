5454066a0a0d2a787d5e2c991a48a807f1236ec1
Jeyavijay N <jeyavijay.n@pluggdd.com>
2017-09-15 11:46:54 +0530

    Initial Commit

---
+++ NurseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NurseProject
+//
+//  Created by Jeyavijay on 15/09/17.
+//  Copyright © 2017 Jeyavijay N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
